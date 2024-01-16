009f742fe96ab24cc47c70705ca6d20471d1c29d
Nodari L <nodari@example.com>
2024-01-16 22:13:11 +0100

    changed ui

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -14,17 +14,22 @@ GainxAudioProcessorEditor::GainxAudioProcessorEditor (GainxAudioProcessor& p)
     : juce::AudioProcessorEditor (&p),
     audioProcessor (p)
 {
-    setSize (200, 300);
-    setResizable(true, true);
+  juce::LookAndFeel_V4::setDefaultLookAndFeel(&gainxLAF);
+  setSize (200, 300);
+  setResizable(true, true);
+  
+  gainSlider.setSliderStyle(juce::Slider::RotaryHorizontalVerticalDrag);
+  gainSlider.setRange(0 , 12.0, 0.1);
+  gainSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 48, 24);
+  gainSlider.setDoubleClickReturnValue(true, 0.0);
+  gainSlider.setTextValueSuffix(" Db");
+  gainSlider.addListener(this);
+  gainSlider.setColour(juce::Slider::rotarySliderFillColourId, juce::Colour(239, 146, 35));
+  gainSlider.setColour(juce::Slider::thumbColourId, juce::Colour(239, 146, 35));
+  gainSlider.setColour(juce::Slider::textBoxTextColourId , juce::Colour(0, 0, 0));
+  gainSlider.setColour(juce::Slider::textBoxOutlineColourId, juce::Colour(226, 226, 226));
 
-    gainSlider.setSliderStyle(juce::Slider::LinearVertical);
-    gainSlider.setRange(-12.0, 12.0, 0.1);
-    gainSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 48, 24);
-    gainSlider.setDoubleClickReturnValue(true, 0.0);
-    gainSlider.setTextValueSuffix(" Db");
-    gainSlider.addListener(this);
-
-    addAndMakeVisible(gainSlider);
+  addAndMakeVisible(gainSlider);
 }
 
 GainxAudioProcessorEditor::~GainxAudioProcessorEditor()
@@ -34,11 +39,12 @@ GainxAudioProcessorEditor::~GainxAudioProcessorEditor()
 //==============================================================================
 void GainxAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+  // (Our component is opaque, so we must completely fill the background with a solid colour)
+  // g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+  g.fillAll(juce::Colour(226, 226, 226));
 
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
+  g.setColour (juce::Colours::white);
+  g.setFont (15.0f);
     // g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
@@ -48,7 +54,7 @@ void GainxAudioProcessorEditor::resized()
     auto height = getHeight();
 
     gainSlider.setBounds(width * 0.06, height * 0.08,
-                         width * 0.24, height * 0.8);
+                         width * 0.5, height * 0.8);
 }
 
 void GainxAudioProcessorEditor::sliderValueChanged(juce::Slider *slider)
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -11,25 +11,28 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
+#include "GainxLookAndFeel.h"
+
 //==============================================================================
 /**
 */
 class GainxAudioProcessorEditor  : public juce::AudioProcessorEditor, public juce::Slider::Listener
 {
 public:
-    GainxAudioProcessorEditor (GainxAudioProcessor&);
-    ~GainxAudioProcessorEditor() override;
+  GainxAudioProcessorEditor (GainxAudioProcessor&);
+  ~GainxAudioProcessorEditor() override;
 
     //==============================================================================
-    void paint (juce::Graphics&) override;
-    void resized() override;
-    void sliderValueChanged(juce::Slider *slider) override;
+  void paint (juce::Graphics&) override;
+  void resized() override;
+  void sliderValueChanged(juce::Slider *slider) override;
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
-    GainxAudioProcessor& audioProcessor;
-    juce::Slider gainSlider;
+  GainxAudioProcessor& audioProcessor;
+  juce::Slider gainSlider;
+  GainxLookAndFeel gainxLAF;
 
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (GainxAudioProcessorEditor)
+  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (GainxAudioProcessorEditor)
 };
